93f03a6b0cd6b6d8bd3b58e81f64605bc7dbc4b5
bezdomnij <attlila.novak@yahoo.co.uk>
2022-06-30 00:14:32 +0200

    refactoring files

--- files.py
+++ files.py
@@ -1,31 +1,56 @@
 from pathlib import Path
 from datetime import datetime
 
+CURRENT_DIR = '2022_05_may'
+
 
 def main():
     rootdir = Path('/Users/frank/pd/Nextcloud')
+
     # Return a list of regular files only, not directories
     file_list = [f for f in rootdir.glob('**/*') if f.is_file()
-                 and f.suffix in ('.xls', '.xlsx', '.XLS', '.XLSX', '.csv', '.CSV', '.txt')
+                 and f.suffix.lower() in ('.xls', '.xlsx', '.csv', '.txt')
                  and 'audit' not in f.stem]
     interesting = [f for f in file_list if 'szamitas' not in str(f)]
 
     # For absolute paths instead of relative the current dir
     # dir_list = [f for f in rootdir.resolve().glob('**/*') if f.is_dir()]
-    dir_list = [f for f in rootdir.glob('**/*') if f.is_dir()]
 
+    list_current_dir_content(rootdir)
+    list_files(interesting)
+
+
+def list_current_dir_content(rootdir):
+    dir_list = [f for f in rootdir.glob('**/*') if f.is_dir()
+                and f.parents[0].name == CURRENT_DIR
+                and f.parents[1].name != 'szamitas']
+    print()
+    for d in dir_list:
+        print(d)
+
+
+def list_files(interesting):
+    """
+    modtimes - a list of tuples: mod_time, file (path object)
+    :param interesting: a list of file path objects that has the monitored suffix
+    :return:
+    """
     modtimes = [(datetime.utcfromtimestamp(int(f.stat().st_mtime)), f) for f in interesting]
     modtimes.sort(reverse=True)
-    dirs = set()
+    print()
     for f in modtimes:
         if f[0].date() > datetime.strptime('2022-01-01', '%Y-%m-%d').date():
             # print(f[0], f[1].parent)
             print(f"{f[0]}, M: {f[1].parents[1].name} | S: {f[1].parents[0].name} | FILE: {f[1].name}")
         # print('*' * 10)
-        measure_timediff(f, store='bookmate')
+        measure_timediff(f, store='bookmate')  # check what happened with Bookmate files
 
 
 def measure_timediff(f, store=''):
+    """
+    old file in a new directory? --> where was it before, was it included in a fin.report before?
+    Bookmate has a tendency to move around
+    """
     if f[0].date() > datetime.strptime('2021-01-01', '%Y-%m-%d').date():
         file_date = f[0]
         dir_date = datetime.utcfromtimestamp(f[1].parent.stat().st_mtime)
